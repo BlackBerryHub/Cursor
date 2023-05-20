8f5584cfa70d9244bda18c38a88f2efe2e1b1afc
BlackBerryHub <49267732+BlackBerryHub@users.noreply.github.com>
2023-05-21 00:08:23 +0300

    Update index.js

--- Lesson 17 - Flask Restful + JS/static/index.js
+++ Lesson 17 - Flask Restful + JS/static/index.js
@@ -10,16 +10,13 @@ fetch("/api/menu-items")
         }
     })
 
-// Assuming you have a target element to append the HTML
 const targetElement = document.getElementById("cards");
 
-// Make a fetch request to retrieve the articles data
 fetch("/api/articles")
     .then(response => response.json())
     .then(articles => {
-        // Loop through each article
         articles.forEach(article => {
-            // Create the card element
+            
             const card = document.createElement("div");
             card.className = "card";
             card.style = "width: 18rem;"
@@ -27,35 +24,30 @@ fetch("/api/articles")
             const cardElement = document.createElement("div");
             cardElement.className = "card-body";
 
-            // Create the card title element
             const titleElement = document.createElement("h5");
             titleElement.className = "card-title";
             titleElement.id = "card_title";
             titleElement.textContent = article.title;
             cardElement.appendChild(titleElement);
 
-            // Create the card date element
             const dateElement = document.createElement("h6");
             dateElement.className = "card-subtitle mb-2 text-body-secondary";
             dateElement.id = "card_date";
             dateElement.textContent = article.date;
             cardElement.appendChild(dateElement);
-
-            // Create the card body element
+            
             const bodyElement = document.createElement("p");
             bodyElement.className = "card-text";
             bodyElement.id = "card_body";
             bodyElement.textContent = article.body;
             cardElement.appendChild(bodyElement);
-
-            // Create the delete link element
+            
             const deleteLinkElement = document.createElement("a");
             deleteLinkElement.href = "/article/" + article.id + "/delete";
             deleteLinkElement.className = "card-link";
             deleteLinkElement.textContent = "Delete";
             cardElement.appendChild(deleteLinkElement);
 
-            // Append the card element to the target element
             card.appendChild(cardElement)
             targetElement.appendChild(card);
         });
