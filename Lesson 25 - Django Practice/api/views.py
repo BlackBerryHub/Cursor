8e1971b7c73eedd8c54a39b85f84ad080f84526c
BlackBerryHub <marat4800@gmail.com>
2023-06-22 00:38:21 +0300

    added api endpoints for orders and added authentication

--- Lesson 25 - Django Practice/api/views.py
+++ Lesson 25 - Django Practice/api/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, OrderSerializer, OrderItemSerializer
 from products.models import Product, Category
-
+from main.models import Order
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 class ProductView(APIView):
 
@@ -64,3 +66,51 @@ class CategoryProductsView(APIView):
                 products = ProductSerializer(category.products, many=True)
                 return Response(products.data)
         return Response(None, status.HTTP_404_NOT_FOUND)
+
+
+class OrderView(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request, format=None):
+        orders = Order.objects.all()
+        serialized_orders = ProductSerializer(orders, many=True)
+        return Response(orders.data)
+
+
+
+class OrderSingleView(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get_object(self, id):
+        try:
+            order = Order.objects.get(id=id)
+            return order
+        except Order.DoesNotExist:
+            return None
+
+
+    def get(self, request, id):
+        order = self.get_object(id)
+        if order is not None:
+            if order.items:
+                items = OrderItemSerializer(order.items, many=True)
+                return Response(items.data)
+        return Response(None, status.HTTP_404_NOT_FOUND)
+
+
+    def put(self, request, id):
+        order = self.get_object(id)
+        if order is not None:
+            serialized_order = OrderSerializer(instance=order, data=request.data)
+            if serialized_order.is_valid():
+                serialized_order.save()
+                return Response(serialized_order.data)
+        return Response(None, status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, id):
+        order = self.get_object(id)
+        if order is not None:
+            order.delete()
+            return Response(None, status.HTTP_204_NO_CONTENT)
+        return Response(None, status.HTTP_400_BAD_REQUEST)
